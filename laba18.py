8d7d48e2c9abcc97b5355cbc6e266291ba356168
Alim <alim-halilko@mail.ru>
2021-05-06 15:29:37 +0300

    18, 22, 24, 25, 27, 28

--- laba18.py
+++ laba18.py
@@ -1,4 +1,20 @@
-stopNames = ["hallo", "klempner", "das", "ist", "fantastisch", "fluggegecheimen"]
-myList = []
+stopWords = ["hallo", "klempner", "das", "ist", "fantastisch", "fluggegecheimen"]
 
-stopWord = input("Введите стоп-слово:")
+lets = {}
+countLet = 0
+
+for let in range(97, 123):
+    lets[chr(let)] = 0
+
+for word in stopWords:
+    for let in word:
+        lets[let] += 1
+        countLet += 1
+
+stopWord = input("Введите стоп слово: ")
+
+chance = 1
+for let in stopWord:
+    chance *= lets[let] / countLet
+
+print(chance)
